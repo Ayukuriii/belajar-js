2a2eb339e73701c245faa35e63b29a039a1b23e0
rsnyn <ekadianharis@gmail.com>
2023-04-03 23:35:35 +0700

    fundamentals (data type)

---
+++ main.js
@@ -0,0 +1,15 @@
+let uname   = 'Ekadian Haris' //data type String
+let age     = 24 //data type number
+let height  = 175.6 //data type double/float
+let weight 
+let hobby   = null
+
+weight  = 175 + "cm"
+
+if(hobby == null){
+    hobby = 'belum ada'
+} else {
+    hobby = 'ada'
+}
+
+alert(`nama saya ${uname} usia saya ${age} tinggi saya ${height} berat saya ${weight} hobi saya ${hobby}`)
