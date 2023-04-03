64d7a2b537e573b701e931b35fe9f5d0398e0634
rsnyn <ekadianharis@gmail.com>
2023-04-03 23:46:07 +0700

    fundamentals (operator)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let uname   = 'Ekadian Haris' //data type String
 let age     = 24 //data type number
 let height  = 175.6 //data type double/float
 let weight 
-let hobby   = null
+let hobby   = 2
 
 weight  = 175 + "cm"
 
@@ -12,4 +12,11 @@ if(hobby == null){
     hobby = 'ada'
 }
 
+// Operator
+let saldoAwal   = 50000
+let saldoTambah = 30000
+const saldoAkhir = saldoAwal + saldoTambah
+
 alert(`nama saya ${uname} usia saya ${age} tinggi saya ${height} berat saya ${weight} hobi saya ${hobby}`)
+
+alert(`saldo awal saya sebesar Rp.${saldoAwal} dan saldo tambahan saya Rp. ${saldoTambah} jadi total saldo saya adalah Rp.${saldoAkhir}`)
