06cefca35eee4f88579a9e940c1f02f4d5a3de61
rsnyn <ekadianharis@gmail.com>
2023-04-04 00:09:52 +0700

    fundamentals (for, do, while loop)

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-// Array 1
-let namaPegawai = ['Atenk', 'Fajar', 'Arip']
-// tambah array
-namaPegawai.push('Haris', 'Uchan') //push untuk menambah ke array
-namaPegawai.shift() // shift unutk menghilangkan awal aray
-namaPegawai.pop() // shift unutk menghilangkan akhir aray
+// Looping
 
-alert(namaPegawai)
+// 1. for loop
+const namaPegawai = ['atenk', 'fajar', 'haris', 'arip']
+console.log(namaPegawai)
 
-// Array 2
-let namaAtasan = []
-    namaAtasan[0] = 'Valdo'
-    namaAtasan[1] = 'Rigel'
-alert(namaAtasan)
+for(let i = 0; i < namaPegawai.length; i++) {
+    console.log(namaPegawai[i]);
+}
+
+// 2. Do While
+let i = 0
+do {
+    i++
+    console.log('Haris')
+} while(i < 5)
